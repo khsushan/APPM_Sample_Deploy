15cafbf01554dcbe53225b3ec23ccd71fbac3b4f
khsushan <khsushan@gmail.com>
2015-03-30 15:21:28 +0530

    update method

--- src/main/java/org/wso2/carbon/appmgt/sampledeployer/appcontroller/ApplicationController.java
+++ src/main/java/org/wso2/carbon/appmgt/sampledeployer/appcontroller/ApplicationController.java
@@ -15,12 +15,26 @@ import org.wso2.carbon.registry.core.exceptions.RegistryException;
 
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.util.Hashtable;
 import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * Created by ushan on 3/27/15.
- */
+/*
+*  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 public class ApplicationController {
 
      final static Logger log = Logger.getLogger(ApplicationController.class.getName());
@@ -32,21 +46,28 @@ public class ApplicationController {
      private int httpPort = 9763;
      private int httpsPort = 9443;
      private WSRegistryService_Client wsRegistryService_client;
-     private String webAppPath ="../../";
+     //private String appmHomePath ="/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT";
+     private String appmHomePath ="../../..";
      private ConcurrentHashMap<String,String> trackingCodes;
      private InvokeStatistcsJavascriptBuilder invokeStatistcsJavascriptBuilder;
      private LoginAdminServiceClient loginAdminServiceClient;
+     private String ipAddress;
+     private String username;
+     private String password;
 
      public  ApplicationController(String username,String password,int offset,String ipAddress) throws IOException, RegistryException, LoginAuthenticationExceptionException {
           httpPort+=offset;
           httpsPort+=offset;
+          this.ipAddress = ipAddress;
+          this.username =  username;
+          this.password =  password;
           httpsBackEndUrl = "https://localhost:"+httpsPort;
-          httpBackEndUrl = "https://localhost:"+httpPort;
+          httpBackEndUrl = "http://localhost:"+httpPort;
           httpHandler = new HttpHandler();
           trackingCodes = new ConcurrentHashMap<String,String>();
           loginAdminServiceClient = new LoginAdminServiceClient(httpsBackEndUrl);
           String serviceSession =loginAdminServiceClient.authenticate(username, password);
-          wsRegistryService_client = new WSRegistryService_Client(serviceSession,httpsBackEndUrl);
+          wsRegistryService_client = new WSRegistryService_Client(httpsBackEndUrl,serviceSession);
           publisherSession = httpHandler.doPostHttps(httpsBackEndUrl + "/publisher/api/authenticate",
                   "username=" + username + "&password=" + password + "&action=login", ""
                   , "application/x-www-form-urlencoded");
@@ -55,36 +76,144 @@ public class ApplicationController {
                           ",\"password\":\"admin\"}", "header", "application/json");
      }
 
-     public void createWebApplication(String serverPort) throws IOException {
-          String policyIDResponce = httpHandler.doPostHttps(httpsBackEndUrl + "/publisher/api/entitlement/policy/partial" +
+     public void manageWebApplication() throws IOException, RegistryException {
+         String policyIDResponce = httpHandler.doPostHttps(httpsBackEndUrl + "/publisher/api/entitlement/policy/partial" +
                           "/policyGroup/save", "anonymousAccessToUrlPattern=false&policyGroupName" +
                           "=test&throttlingTier=Unlimited&objPartialMappings=[]&policyGroupDesc=null&userRoles=",
                   publisherSession, "application/x-www-form-urlencoded; charset=UTF-8").split(":")[3];
-          String policyId = policyIDResponce.substring(1, (policyIDResponce.length() - 2)).trim();
-          AppCreateRequest appCreateRequest = new AppCreateRequest();
-          appCreateRequest.setUritemplate_policyGroupIds("[" + policyId + "]");
-          appCreateRequest.setUritemplate_policyGroupId4(policyId);
-          appCreateRequest.setUritemplate_policyGroupId3(policyId);
-          appCreateRequest.setUritemplate_policyGroupId2(policyId);
-          appCreateRequest.setUritemplate_policyGroupId1(policyId);
-          appCreateRequest.setUritemplate_policyGroupId0(policyId);
-          appCreateRequest.setClaimPropertyName0("http://wso2.org/claims/streetaddress,http://wso2.org/ffid" +
+         String policyId = policyIDResponce.substring(1, (policyIDResponce.length() - 2)).trim();
+         AppCreateRequest appCreateRequest = new AppCreateRequest();
+         appCreateRequest.setUritemplate_policyGroupIds("[" + policyId + "]");
+         appCreateRequest.setUritemplate_policyGroupId4(policyId);
+         appCreateRequest.setUritemplate_policyGroupId3(policyId);
+         appCreateRequest.setUritemplate_policyGroupId2(policyId);
+         appCreateRequest.setUritemplate_policyGroupId1(policyId);
+         appCreateRequest.setUritemplate_policyGroupId0(policyId);
+         appCreateRequest.setClaimPropertyName0("http://wso2.org/claims/streetaddress,http://wso2.org/ffid" +
                   ",http://wso2.org/claims/telephone");
-          appCreateRequest.setClaimPropertyCounter("3");
+         appCreateRequest.setClaimPropertyCounter("3");
           //publishing travelWebapp
-          log.info("publishing travleWebapp");
-          appCreateRequest.setOverview_name("travelWebapp");
-          appCreateRequest.setOverview_displayName("travelWebapp");
-          appCreateRequest.setOverview_context("/travel");
-          appCreateRequest.setOverview_version("1.0.0");
-          appCreateRequest.setOverview_trackingCode(appCreateRequest.generateTrackingID());
-          appCreateRequest.setOverview_transports("http");
-          appCreateRequest.setOverview_webAppUrl("http://localhost:" +serverPort + "/plan-your-trip-1.0/");
-          String payload = appCreateRequest.generateRequestParameters();
+         log.info("publishing travleWebapp");
+         appCreateRequest.setOverview_name("travelWebapp");
+         appCreateRequest.setOverview_displayName("travelWebapp");
+         appCreateRequest.setOverview_context("/travel");
+         appCreateRequest.setOverview_version("1.0.0");
+         appCreateRequest.setOverview_trackingCode(appCreateRequest.generateTrackingID());
+         appCreateRequest.setOverview_transports("http");
+         appCreateRequest.setOverview_webAppUrl("http://localhost:" +httpPort + "/plan-your-trip-1.0/");
+         String UUID = createWebApplication(appCreateRequest);
+         publishApplication("webapp", UUID);
+         log.info(appCreateRequest.getOverview_name()+" published and UUID is "+UUID);
+         subscribeApplication(appCreateRequest);
+         log.info(appCreateRequest.getOverview_name()+"application subscribed by user "+username);
+         //publishing travel booking application
+         log.info("publishing travleWebapp");
+         appCreateRequest.setOverview_name("TravelBooking");
+         appCreateRequest.setOverview_displayName("TravelBooking");
+         appCreateRequest.setOverview_context("/travelBooking");
+         appCreateRequest.setOverview_version("1.0.0");
+         appCreateRequest.setOverview_transports("http");
+         appCreateRequest.setOverview_trackingCode(appCreateRequest.generateTrackingID());
+         appCreateRequest.setClaimPropertyName0("http://wso2.org/claims/givenname,http://wso2.org/claims/lastname" +
+                 ",http://wso2.org/claims/emailaddress,http://wso2.org/claims/streetaddress" +
+                 ",http://wso2.org/claims/zipcode,http://wso2.org/claims/country" +
+                 ",http://wso2.org/claims/card_number,http://wso2.org/claims/card_holder" +
+                 ",http://wso2.org/claims/expiration_date");
+         appCreateRequest.setClaimPropertyCounter("9");
+         appCreateRequest.setOverview_webAppUrl("http://localhost:" + httpPort + "/travel-booking-1.0/");
+         UUID = createWebApplication(appCreateRequest);
+         publishApplication("webapp",UUID);
+         log.info(appCreateRequest.getOverview_name()+" published and UUID is "+UUID);
+         subscribeApplication(appCreateRequest);
+         log.info(appCreateRequest.getOverview_name()+"application subscribed by user "+username);
+     }
+
+     public void manageMobilebApplication() throws IOException, InterruptedException {
+         log.info("publishing CleanCalc mobile application");
+         MobileApplicationBean mobileApplicationBean = new MobileApplicationBean();
+         mobileApplicationBean.setApkFile(appmHomePath+"/repository/resources/mobileapps/CleanCalc" +
+                 "/Resources/CleanCalc.apk");
+         String appMeta = httpHandler.doPostMultiData(httpsBackEndUrl + "/publisher/api/mobileapp/upload",
+                 "upload", mobileApplicationBean,publisherSession);
+         mobileApplicationBean.setVersion("1.0.0");
+         mobileApplicationBean.setProvider("1WSO2Mobile");
+         mobileApplicationBean.setMarkettype("Enterprise");
+         mobileApplicationBean.setPlatform("android");
+         mobileApplicationBean.setName("CleanCalc");
+         mobileApplicationBean.setDescription("this is a clean calucultor");
+         mobileApplicationBean.setBannerFilePath(appmHomePath+"/repository/resources/mobileapps/" +
+                 "CleanCalc/Resources/banner.png");
+         mobileApplicationBean.setIconFile(appmHomePath+ "/repository/resources/mobileapps/CleanCalc" +
+                 "/Resources/icon.png");
+         mobileApplicationBean.setScreenShot1File(appmHomePath + "/repository/resources/mobileapps/CleanCalc" +
+                 "/Resources/screen1.png");
+         mobileApplicationBean.setScreenShot2File(appmHomePath + "/repository/resources/mobileapps/CleanCalc" +
+                 "/Resources/screen2.png");
+         mobileApplicationBean.setScreenShot3File(appmHomePath+ "/repository/resources/mobileapps/CleanCalc" +
+                 "/Resources/screen3.png");
+         mobileApplicationBean.setMobileapp("mobileapp");
+         mobileApplicationBean.setAppmeta(appMeta);
+         String UUID = createMobielAppliaction(mobileApplicationBean);
+         publishApplication("mobileapp",UUID);
+         //wso2Con mobile application
+         log.info("publishing WSO2Con mobile application");
+         mobileApplicationBean.setAppmeta("{\"package\":\"com.wso2.wso2con.mobile\",\"version\":\"1.0.0\"}");
+         mobileApplicationBean.setVersion("1.0.0");
+         mobileApplicationBean.setProvider("1WSO2Mobile");
+         mobileApplicationBean.setMarkettype("Market");
+         mobileApplicationBean.setPlatform("android");
+         mobileApplicationBean.setName("Wso2Con");
+         mobileApplicationBean.setDescription("WSO2Con mobile app provides the agenda and meeting tool for " +
+                 "WSO2Con. Get the app to follow the agenda, find out about the sessions and speakers, provide " +
+                 "feedback on the sessions, and get more information on the venue and sponsors. Join us at " +
+                 "WSO2Con, where we will place emerging technology, best practices, and WSO2 product features " +
+                 "in the perspective of accelerating development and building a connected business. We hope you " +
+                 "enjoy WSO2Con!");
+         mobileApplicationBean.setBannerFilePath(appmHomePath+ "/repository/resources/mobileapps/WSO2Con" +
+                 "/Resources/banner.png");
+         mobileApplicationBean.setIconFile(appmHomePath + "/repository/resources/mobileapps/WSO2Con" +
+                 "/Resources/icon.png");
+         mobileApplicationBean.setScreenShot1File(appmHomePath + "/repository/resources/mobileapps/WSO2Con" +
+                 "/Resources/screen1.png");
+         mobileApplicationBean.setScreenShot2File(appmHomePath + "/repository/resources/mobileapps" +
+                 "/WSO2Con/Resources/screen2.png");
+         mobileApplicationBean.setMobileapp("mobileapp");
+         UUID = createMobielAppliaction(mobileApplicationBean);
+         publishApplication("mobileapp",UUID);
+         //MyTrack mobile application
+         log.info("publishing MyTrack mobile application");
+         mobileApplicationBean.setAppmeta("{\"package\":\"com.google.android.maps.mytracks\",\"version\":\"1.0" +
+                 ".0\"}");
+         mobileApplicationBean.setVersion("1.0.0");
+         mobileApplicationBean.setProvider("1WSO2Mobile");
+         mobileApplicationBean.setMarkettype("Market");
+         mobileApplicationBean.setPlatform("android");
+         mobileApplicationBean.setName("MyTracks");
+         mobileApplicationBean.setDescription("My Tracks records your path, speed, distance, and elevation while " +
+                 "you walk, run, bike, or do anything else outdoors. While recording, you can view your data live" +
+                 ", annotate your path, and hear periodic voice announcements of your progress.\n" +
+                 "With My Tracks, you can sync and share your tracks via Google Drive. For sharing, you can share" +
+                 " tracks with friends, see the tracks your friends have shared with you, or make tracks public " +
+                 "and share their URLs via Google+, Facebook, Twitter, etc. In addition to Google Drive, you can " +
+                 "also export your tracks to Google My Maps, Google Spreadsheets, or external storage.\n" +
+                 "My Tracks also supports Android Wear. For watches with GPS, My Tracks can perform GPS recording" +
+                 " of tracks without a phone and sync tracks to the phone. For watches without GPS, you can see " +
+                 "your current distance and time, and control the recording of your tracks from your wrist.");
+         mobileApplicationBean.setBannerFilePath(appmHomePath + "/repository/resources/" +
+                 "mobileapps/MyTracks/Resources/banner.png");
+         mobileApplicationBean.setIconFile(appmHomePath + "/repository/resources/mobileapps" +
+                 "/MyTracks/Resources/icon.png");
+         mobileApplicationBean.setScreenShot1File(appmHomePath + "/repository/resources/mobileapps/MyTracks" +
+                 "/Resources/screen1.png");
+         mobileApplicationBean.setScreenShot2File(appmHomePath + "/repository/resources/mobileapps/MyTracks" +
+                 "/Resources/screen2.png");
+         mobileApplicationBean.setMobileapp("mobileapp");
+         UUID = createMobielAppliaction(mobileApplicationBean);
+         publishApplication("mobileapp",UUID);
 
      }
 
-     private void deployWebApplication(AppCreateRequest appCreateRequest) throws IOException, RegistryException {
+     private String createWebApplication(AppCreateRequest appCreateRequest) throws IOException, RegistryException {
           String payload = appCreateRequest.generateRequestParameters();
           httpHandler.doPostHttps(httpsBackEndUrl + "/publisher/asset/webapp", payload
                   ,publisherSession, "application/x-www-form-urlencoded");
@@ -118,18 +247,18 @@ public class ApplicationController {
 
          trackingCodes.put(appCreateRequest.getOverview_context(), trackingID);
          invokeStatistcsJavascriptBuilder = new InvokeStatistcsJavascriptBuilder
-                 (trackingID,"");//ip address to be implement
-
+                 (trackingID,ipAddress);//ip address to be implement
           if (appCreateRequest.getOverview_name().equals("travelWebapp")) {
-               invokeStatistcsJavascriptBuilder.buildInvokeStaticsJavascriptFile("webapppath"+//to be implement
-                       "/webapps/plan-your-trip-1.0");
+              invokeStatistcsJavascriptBuilder.buildInvokeStaticsJavascriptFile(appmHomePath+
+                      "/repository/deployment/server/webapps/plan-your-trip-1.0");
           } else if (appCreateRequest.getOverview_name().equals("TravelBooking")) {
-               invokeStatistcsJavascriptBuilder.buildInvokeStaticsJavascriptFile("webapp path"+ //to be implement
-                       "/webapps/travel-booking-1.0/js");
+              invokeStatistcsJavascriptBuilder.buildInvokeStaticsJavascriptFile(appmHomePath+
+                       "/repository/deployment/server/webapps/travel-booking-1.0/js");
           }
+         return  UUID;
      }
 
-     public  void publishApplication(String applicationType,String applicationName,String UUID) throws IOException {
+     private  void publishApplication(String applicationType,String UUID) throws IOException {
           httpHandler.doPut(httpsBackEndUrl + "/publisher/api/lifecycle/Submit%20for%20Review/" + applicationType + "/" + UUID
                   , publisherSession);
 
@@ -138,10 +267,10 @@ public class ApplicationController {
 
           httpHandler.doPut(httpsBackEndUrl + "/publisher/api/lifecycle/Publish/" + applicationType + "/" + UUID
                   , publisherSession);
-          log.info(applicationName+" "+applicationType+" published and UUID is "+UUID);
+
      }
 
-     public  void subscribeApplication(AppCreateRequest appCreateRequest) throws IOException {
+     private  void subscribeApplication(AppCreateRequest appCreateRequest) throws IOException {
           httpHandler.doPostHttps(httpsBackEndUrl + "/store/resources/webapp/v1/subscription/app",
                   "apiName=" + appCreateRequest.getOverview_name() + "" +
                           "&apiVersion=" + appCreateRequest.getOverview_version() + "&apiTier=" +
@@ -150,13 +279,11 @@ public class ApplicationController {
                   , storeSession, "application/x-www-form-urlencoded; charset=UTF-8");
      }
 
-     public  String createMobielAppliaction(MobileApplicationBean mobileApplicationBean) throws IOException {
-          if(mobileApplicationBean.getName().equals("CleanCalc")){
-               httpHandler.doPostMultiData(httpsBackEndUrl + "/publisher/api/mobileapp/upload",
-                       "upload", mobileApplicationBean, publisherSession);
-          }
-          return httpHandler.doPostMultiData(httpsBackEndUrl + "/publisher/api/asset/mobileapp", "none",
-                  mobileApplicationBean, publisherSession);
+
+
+     private String createMobielAppliaction(MobileApplicationBean mobileApplicationBean) throws IOException, InterruptedException {
+         return httpHandler.doPostMultiData(httpsBackEndUrl + "/publisher/api/asset/mobileapp", "none",
+                 mobileApplicationBean, publisherSession);
      }
 
     public void accsesWebPages(String webContext, String trackingCode, int hitCount,String ipAddress) {
