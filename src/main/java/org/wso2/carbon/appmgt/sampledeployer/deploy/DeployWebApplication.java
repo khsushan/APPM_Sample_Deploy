6db39d04496499448e765448fbbb1499e6e0bdd4
khsushan <khsushan@gmail.com>
2015-03-30 15:19:28 +0530

    add appHomePath variable

--- src/main/java/org/wso2/carbon/appmgt/sampledeployer/deploy/DeployWebApplication.java
+++ src/main/java/org/wso2/carbon/appmgt/sampledeployer/deploy/DeployWebApplication.java
@@ -1,47 +1,39 @@
 package org.wso2.carbon.appmgt.sampledeployer.deploy;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
+import java.nio.channels.FileChannel;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
-/**
- * Created by ushan on 3/27/15.
- */
+/*
+*  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 public class DeployWebApplication {
+    //private static String homePath = "/home/ushan/Shell_Script_Test/APPM/wso2appm-1.0.0-SNAPSHOT";
+    private static String homePath = "../../..";
 
-    public void unZipFile(String zipFilePath, String destDirectory) throws IOException {
-        File destDir = new File(destDirectory);
-        if (!destDir.exists()) {
-            destDir.mkdir();
-        }
-        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
-        ZipEntry entry = zipIn.getNextEntry();
-        // iterates over entries in the zip file
-        while (entry != null) {
-            String filePath = destDirectory + File.separator + entry.getName();
-            if (!entry.isDirectory()) {
-                // if the entry is a file, extracts it
-                extractFile(zipIn, filePath);
-            } else {
-                // if the entry is a directory, make the directory
-                File dir = new File(filePath);
-                dir.mkdir();
-            }
-            zipIn.closeEntry();
-            entry = zipIn.getNextEntry();
-        }
-        zipIn.close();
+    public static void copyFileUsingFileStreams(String warFileName) throws IOException {
+        File souceFile  = new File(homePath+"/samples/"+warFileName);
+        File destinantionFile = new File(homePath+"/repository/deployment/server/webapps/"+warFileName);
+        FileUtils.copyFile(souceFile,
+                destinantionFile);
     }
-
-    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException{
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
-        byte[] bytesIn = new byte[1024];
-        int read = 0;
-        while ((read = zipIn.read(bytesIn)) != -1) {
-            bos.write(bytesIn, 0, read);
-        }
-        bos.close();
-    }
-
-
 }
